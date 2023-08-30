86b4ba30fe1fc5baeff2f3c1e3db6f5a4322ab8e
Jz5zp <73933741+Jz5zp@users.noreply.github.com>
2023-08-30 17:11:34 +1000

    Update app.py with joined table

--- SuburbScape/Backend/app.py
+++ SuburbScape/Backend/app.py
@@ -1,55 +1,44 @@
 import pyodbc
 from flask import Flask, jsonify
-from flask_cors import CORS
 app = Flask(__name__)
-CORS(app)  # Enable CORS for all routes
 
-# Azure SQL
+# Azure SQL connection
+#https://devmaverick.azurewebsites.net/api/data
 server = 'maverick.database.windows.net'
-database = 'dev'
+database = 'dev' 
 username = 'ta04'
 password = 'Zty912815'
-driver = '{ODBC Driver 18 for SQL Server}'
+driver = '{ODBC Driver 18 for SQL Server}'  
 
-@app.route('/', methods=['GET'])
-def get_data():
-    data = {'message': 'Hello from Flask API'}
-    return jsonify(data)
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
-@app.route('/api/rent_data')
+@app.route('/api/data')
 def get_rent_data():
     try:
         data = []
 
+        # connection 
         connection_string = f"DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}"
         conn = pyodbc.connect(connection_string)
+
+        # create cursor
         cursor = conn.cursor()
-        cursor.execute("SELECT * FROM rent_vF")
+
+        # select all
+        cursor.execute("SELECT * FROM join_dataset")  # Database Name
         rows = cursor.fetchall()
+
+        # json format data API
         for row in rows:
-            data.append({'Region': row[0], 'LGA': row[1], 'Count': row[2], 'Median': row[3], 'Description': row[4]})
+            data.append({'LGA': row[0], 'Offense_Count': row[1], 'Rate_per_100000_population': row[2], 'Median': row[3], 'Description': row[4]})
+
+        # Close cursor and connection
         cursor.close()
         conn.close()
-        return jsonify(data)
 
-    except pyodbc.Error as e:
-        error_message = str(e)
-        print("An error occurred:", error_message)
-        return jsonify({'error': error_message}), 500
-
-@app.route('/api/crime_data')
-def get_crime_data():
-    try:
-        data = []
-        connection_string = f"DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}"
-        conn = pyodbc.connect(connection_string)
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM crime_vF")
-        rows = cursor.fetchall()
-        for row in rows:
-            data.append({'LGA': row[0], 'Category': row[1], 'Offence_Count': row[2], 'Rate_per_100000_population': row[3]})
-        cursor.close()
-        conn.close()
+        # Json response
         return jsonify(data)
 
     except pyodbc.Error as e:
